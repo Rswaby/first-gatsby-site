55f1eb5df691b2ab5a850ec0f72edd500af3fb68
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-10-17 17:12:02 -0400

    otherfiles

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,5 @@ export default () =>
     <div>
         <header>Hello world!</header>
         <Link to="/somepage"> some page </Link>
+        <Link to="otherfiles/otherfiles">otherfile</Link>
     </div>
---
+++ src/pages/otherfiles/otherfiles.js
@@ -0,0 +1,7 @@
+import React from "react"
+import Link from "gatsby-link"
+export default () =>
+    <div>
+        <header>Other files!</header>
+        <Link to="/"> Home </Link>
+    </div>
