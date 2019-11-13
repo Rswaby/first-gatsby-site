aa546a2e3e71a7e171bb1199769cc7edaa1e5d2a
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-13 14:36:34 -0500

    removing transition

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+// import { TransitionPortal } from "gatsby-plugin-transition-link";
 
 import Header from "./header"
 import "./layout.css"
@@ -34,6 +35,7 @@ const Layout = ({ children }) => {
           paddingTop: 0,
         }}
       >
+      
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-
+// import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link  to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
