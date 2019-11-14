fae80b2fe727399de988ff18f070b1ccd964e493
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-13 19:12:38 -0500

    clean kup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Link } from "gatsby"
-// import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
