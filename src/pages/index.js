f88c81061a7d6d3ef54fb0f831f4eaad5e98fd81
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-13 19:27:11 -0500

    mardown add

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import ReactMarkdown from 'react-markdown';
+const input = 'community cabin least older simplest public prepare can feathers dull chose development date smaller roar pot diameter baseball replace search doubt floating airplane cattle'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <ReactMarkdown source={input}/>
     <div>
       <Image />
     </div>
