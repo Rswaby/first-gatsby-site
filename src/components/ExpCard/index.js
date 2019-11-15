bc11040b4fd2f4778b7e9fe308c92d31eb5d60d8
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-14 20:18:14 -0500

    css experience

--- src/components/ExpCard/index.js
+++ src/components/ExpCard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ExpCard = ({ExpData}) => {
-    return ( <div></div> );
+    return ( <div>{ExpData}</div> );
 }
  
 export default ExpCard;
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -2,11 +2,13 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import ExpCard from "../components/ExpCard"
 import SEO from "../components/seo"
 
 const experience = () => (
     <Layout>
         <SEO title="experience" />
+        <ExpCard ExpData={"ExpCard Data"}/>
         <p>Welcome to experience page</p>
         <Link to="/">Go back to the homepage</Link>
     </Layout>
