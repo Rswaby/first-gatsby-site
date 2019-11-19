402b6c7775dd4f1e873b7e0f1a5c7be59dba4a41
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-18 20:18:59 -0500

    xp page

--- src/components/ExpCard/index.js
+++ src/components/ExpCard/index.js
@@ -4,14 +4,14 @@ const ExpCard = ({ ExpData }) => {
     return (
         <div className={"myCard"}>
             <div>
-                {ExpData}
+                <p>{ExpData}</p>
             </div>
             <div>
-                <img src="https://animesouls.com/wp-content/uploads/2017/03/14.jpg"
+                <img src="https://static.toiimg.com/photo/msid-67868104/67868104.jpg?1368689"
                     alt={"sasuke"}
-                    width={"100%"}
-                    height={"100%"}
-                    style={{ marginBottom: 0 }}
+                    width={"500px"}
+                    height={"315px"}
+                    style={{ marginBottom: 0,display:"block",textAlign:"center" }}
                 ></img>
             </div>
         </div>
