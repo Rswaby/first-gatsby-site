eddbfdefa498dd1fc7267accaef3a44906f755c0
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-11-25 23:35:52 -0500

    setting up project cards grids

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,11 +3,17 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Grids from '../components/subLayout/Grids';
+
 
 const projects = () => (
     <Layout>
         <SEO title="projects" />
         <p>Welcome to projects page</p>
+        <Grids>
+            <p>projects Profile cards</p>
+        </Grids>
+
         <Link to="/">Go back to the homepage</Link>
     </Layout>
 )
