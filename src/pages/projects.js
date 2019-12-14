c143842aad0f0e238b77e77a9386d1c9d557662b
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-13 23:23:38 -0500

    added more data

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,6 +7,8 @@ import Grids from '../components/subLayout/Grids';
 import ProjCard from '../components/ProjCards'
 
 const Data = [
+    'melted time youth average visit alone them could sense owner trade in twenty production threw away letter plural desk willing easy person let anything',
+    'becoming oil break play happened hunt jar excitement use silk somewhere whistle flower properly thou concerned whale birth fort elephant anyone pattern problem tight',
     '<ProjCard Data="Project Cards Go here" /> exercise porch private signal consist cotton held kept pond quietly sheet garden giant carry change breath field main tonight high tide allow article on airplane pond golden softly she stronger gasoline seed tall occur easy protection trace choose firm section positive ran floor handsome minute missing explanation victory',
      'telephone distant has guide exercise glass freedom hit everything rough slide highway standard progress recall doctor fuel too occasionally look but glad become harder swing especially development lion meal living useful seen equator difference determine gas lead upward jungle conversation heat fireplace depth young only image his situation',
     'vertical why hollow spend follow hay time replace spider clean golden constantly have few account themselves smallest fur threw ear again bridge trap tree look held scientific breathe son rule thrown silent climb represent plenty setting breeze increase system card saved where new stepped classroom indeed wagon tent',
