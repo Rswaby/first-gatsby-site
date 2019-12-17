69122caedaad63695f1998a807f61ff76db8ad21
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-16 23:02:59 -0500

    added grid to project page

--- src/components/ProjCards/index.js
+++ src/components/ProjCards/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 const ProjCard = ({ Data }) => {
     return (
         <div>
-            {Data}
+            <div>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxu3FrWCOi59a-Zems8rHMPaxxsKThUlRg6-ZOGe9fFgmMblQi&s"
+                    alt="stock"
+                />
+                <div>technologies</div>
+            </div>
+            <p>{Data}</p>
         </div>
     );
 }
