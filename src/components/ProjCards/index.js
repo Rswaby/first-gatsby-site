838a8a5b4e098afdc2a9814c35bd2a12aa38ff45
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-17 22:29:22 -0500

    added more mock data

--- src/components/ProjCards/index.js
+++ src/components/ProjCards/index.js
@@ -7,7 +7,14 @@ const ProjCard = ({ Data }) => {
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxu3FrWCOi59a-Zems8rHMPaxxsKThUlRg6-ZOGe9fFgmMblQi&s"
                     alt="stock"
                 />
-                <div>technologies</div>
+                <div>
+                    <h5>Stack</h5>
+                    <lu>
+                        <li>Java</li>
+                        <li>StringBoot</li>
+                        <li>mySql</li>
+                    </lu>
+                </div>
             </div>
             <p>{Data}</p>
         </div>
