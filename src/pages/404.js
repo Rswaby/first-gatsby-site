d54234e341947b999b63904e9a932331a1989256
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-23 22:53:23 -0500

    redid 404 file

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,28 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <figure>
-      <div className="sad-mac"></div>
-      <figcaption>
-        <span className="sr-text">Error 404: Not Found</span>
-        <span className="e"></span>
-        <span className="r"></span>
-        <span className="r"></span>
-        <span className="o"></span>
-        <span className="r"></span>
-        <span className="_4"></span>
-        <span className="_0"></span>
-        <span className="_4"></span>
-        <span className="n"></span>
-        <span className="o"></span>
-        <span className="t"></span>
-        <span className="f"></span>
-        <span className="o"></span>
-        <span className="u"></span>
-        <span className="n"></span>
-        <span className="d"></span>
-      </figcaption>
-    </figure>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
 
