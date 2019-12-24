e150a3fbcc6e753213ae88c46243e7301996a590
Rohan Swaby <rohan_swaby51@yahoo.com>
2019-12-23 22:25:56 -0500

    deleted 404 file contents

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,28 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
+    <figure>
+      <div class="sad-mac"></div>
+      <figcaption>
+        <span class="sr-text">Error 404: Not Found</span>
+        <span class="e"></span>
+        <span class="r"></span>
+        <span class="r"></span>
+        <span class="o"></span>
+        <span class="r"></span>
+        <span class="_4"></span>
+        <span class="_0"></span>
+        <span class="_4"></span>
+        <span class="n"></span>
+        <span class="o"></span>
+        <span class="t"></span>
+        <span class="f"></span>
+        <span class="o"></span>
+        <span class="u"></span>
+        <span class="n"></span>
+        <span class="d"></span>
+      </figcaption>
+    </figure>
   </Layout>
 )
 
