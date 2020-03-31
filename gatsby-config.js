94dc1a34cbda0614fedeef989c929b5fadfcec49
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-03-30 22:08:38 -0400

    gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,15 +6,35 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    //my stuff
     `gatsby-plugin-transition-link`,
-    //my stuff;
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    // You can have multiple instances of this plugin
+    // to read source nodes from different locations on your
+    // filesystem.
+    //
+    // The following sets up the Jekyll pattern of having a
+    // "pages" directory for Markdown files and a "data" directory
+    // for `.json`, `.yaml`, `.csv`.
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
       },
     },
     `gatsby-transformer-sharp`,
