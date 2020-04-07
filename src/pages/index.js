3874aed36428b1769251040e7b9a12c01621b38e
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-04-06 23:33:47 -0400

    Data source

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,20 +5,18 @@ import SEO from "../components/seo"
 import { Emojione } from 'react-emoji-render';
 import moment from 'moment';
 import '../css/index.css';
+import JSONData from "../../src/data/json-data/home-page.json"
 const input = 'Go ahead and have look around. :smiley: :rocket: '
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
 
     <div className={"center m-top-30"}>
-      <img className="rohan" src="https://randomuser.me/api/portraits/lego/0.jpg" alt="rohan" />
+      <img className="rohan" src={JSONData.content.item.pic} alt="rohan" />
       <h2>Hi, {getGreetingTime(moment())}</h2>
-      <h3>I'm Rohan, a</h3>
-      <h3>Software Engineer.</h3>
-      <p>I am a recent grad from The City College Of New York.</p>
-      <p>I have over 5 years of coding experience with Python, Java and JS.</p>
-      <p>happen matter closely government whose bring weight card lie off arrow bus would <br />
-        alone outside grown flow sun chair around increase religious reader above</p>
+      <h3>I'm {JSONData.content.item.name}, a</h3>
+      <h3>{JSONData.content.item.acc}.</h3>
+      <p>{JSONData.content.item.details}</p>
       <Emojione svg text={input} />
       <br />
       <Link to="/experience">
