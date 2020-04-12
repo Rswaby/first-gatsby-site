a302d46d2bf4a434b87dfbaf28222e2f6e2a6992
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-04-11 21:00:34 -0400

    souce file Exp-data

--- src/components/ExpCard/index.js
+++ src/components/ExpCard/index.js
@@ -1,29 +1,31 @@
 import React from 'react';
-// import Image from '../image'
+import ReactMarkdown from 'react-markdown'
 const ExpCard = ({ ExpData }) => {
     return (
         <div className={"myCard"}>
+            {console.log(ExpData.item)}
+
             <div>
                 <div className={"flex-container"}>
-                    <h4 className={"company-title"}>Company Name</h4>
-                    <label>Jan 2018 - Jan 2019</label>
+                    <h4 className={"company-title"}>{ExpData.item.companyName}</h4>
+                    <label>{ExpData.item.date}</label>
                 </div>
-                <p>{ExpData}</p>
+                <label>{ExpData.item.position}</label>
+                <ReactMarkdown source={ExpData.item.description} />
                 <div className={"company-tech"}>
                     {
-                        [1, 2, 3, 4, 5].map((l, index) =>
-                            <label className={"tech-label"}>
-                                <span style={{ padding: '5px' }}> tech{index} </span>
+                        ExpData.item.techs.map((tech, index) =>
+                            <label key={tech + "ExpCard" + index} className={"tech-label"}>
+                                <span style={{ padding: '5px' }}> {tech} </span>
                             </label>
                         )
                     }
                 </div>
             </div>
             <div>
-                <img src="https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg"
+                <img src={ExpData.item.imgurl}
                     alt={"sasuke"}
                     width={"500px"}
-                    height={"315px"}
                     style={{ marginBottom: 0, display: "block", textAlign: "center" }}
                 ></img>
             </div>
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -5,15 +5,15 @@ import Layout from "../components/layout"
 import ExpCard from "../components/ExpCard"
 import SEO from "../components/seo"
 import Grids from '../components/subLayout/Grids';
-const data = [1, 2, 3, 5]
+import JSONData from "../data/json-data/Exp-page.json";
+
 const experience = () => (
     <Layout>
         <SEO title="experience" />
-        <h3 className="mtop20">Experience.</h3>
+        <h3 className="mtop20">{JSONData.title}</h3>
         <Grids>
-
             {
-                data.map((d) => <ExpCard ExpData={d + "progress tribe globe chamber bush successful rich new upper hour worry environment engineer its down certainly choose occur western wear blood sharp larger electricity mathematics aid deer machine pink fighting kids thou topic her thousand familiar pipe fourth pick for does palace trail ask understanding human torn situation feet brother central unit rule lift blow color pie smell soft shorter still basic spoken silly count jar buried right somehow saddle nearest spider"} />)
+                JSONData.content.map((data,d) => <ExpCard key={d.toString()} ExpData={data} />)
             }
         </Grids>
     </Layout>
